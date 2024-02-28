f00ca55bbff2c4572edcb57502bccd102104533a
mohamed <medosas113@gmail>
2024-02-28 18:19:51 +0200

    Style_File

---
+++ utils/styles.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+// R= regular
+// B=bold
+abstract class Styles {
+  static const text18R = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+  static const text20R = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+  static const text22R = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.normal,
+  );
+  static const text26R = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.normal,
+  );
+  static const text22B = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+  );
+  static const text24B = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+}
